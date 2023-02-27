98b3da76f5e7b4fefedb6a7556755a528be99ed1
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-02-27 10:15:52 +0530

    Create longestCommonSubstringO(N).py

---
+++ longestCommonSubstringO(N).py
@@ -0,0 +1,21 @@
+def lcs(X, Y, m, n):
+    dp = [[0] * (n + 1) for _ in range(2)]
+    result = 0 
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                dp[i % 2][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                dp[i % 2][j] = dp[(i - 1) % 2][j - 1] + 1
+                if dp[i % 2][j] > result:
+                    result = dp[i % 2][j]
+                result = max(result, dp[i][j])
+            else:
+                dp[i % 2][j] = 0
+
+    print(result)
+
+if __name__ == '__main__':
+    x, y = input().split()
+    lcs(x, y, len(x), len(y))
