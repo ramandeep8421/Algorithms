41b75dd7ac4d9bdfc4335a09437822851911fda4
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-03-05 15:18:49 +0530

    Update minSumPath.java

--- minSumPath.java
+++ minSumPath.java
@@ -1,35 +1,49 @@
 import java.util.*;
 
 public class Main {
-    public static int coinChange(int idx, List<Integer> coins, int amount) {
-        // If the number of coins are exhausted
-        if (idx >= coins.size() || amount <= 0) return 0;
-
-        int res = -1;
-        // If the value of coin at current location
-        // is greater than amount
-        if (coins.get(idx) > amount) {
-            // Case where we are not taking the current coin
-            int doNotTakeCoin = coinChange(idx+1, coins, amount);
-            res = doNotTakeCoin;
-        } else {
-            // Two cases are there
-            // case-1: where we take the coin
-            // case-2: where we do not take the coin
-            int takeCoin = 1 + coinChange(idx, coins, amount-coins.get(idx));
-            int doNotTakeCoin = 0 + coinChange(idx+1, coins, amount);
-            res = Math.min(takeCoin, doNotTakeCoin);
+    static int minPathSum(int i, int j, int[][] grid, int[][] dp) {
+        // Base case-1
+        if (i == 0 && j == 0) {
+            return grid[i][j];
         }
 
-        return res;
+        // Base case-2
+        if (i < 0 || j < 0) {
+            return 10000007;
+        }
+
+        // Memoization case
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        // Recur for the path in 0 to i-1
+        int up = grid[i][j] + minPathSum(i-1, j, grid, dp);
+
+        // Recur for the path in 0 to j-1
+        int left = grid[i][j] + minPathSum(i, j-1, grid, dp);
+
+        // Return the result
+        return dp[i][j] = Math.min(up, left);
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int amount = scanner.nextInt();
-        List<Integer> coins = new ArrayList<>();
-        for (int i=0; i<n; i++) coins.add(scanner.nextInt());
-        System.out.println(coinChange(0, coins, amount));
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] grid = new int[n][m];
+        int[][] dp = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                grid[i][j] = sc.nextInt();
+                dp[i][j] = -1;
+            }
+        }
+
+        System.out.println(minPathSum(n-1, m-1, grid, dp));
     }
 }
+
