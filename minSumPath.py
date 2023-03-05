7e962b765d6ba74e0552d3df74c02ec330ae763b
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-03-05 15:17:39 +0530

    Update minSumPath.py

--- minSumPath.py
+++ minSumPath.py
@@ -1,26 +1,34 @@
-def coinChange(idx, coins, amount):
-    # If the number of coins are exhausted
-    if idx >= len(coins) or amount <= 0:
-        return 0
+import sys
+sys.setrecursionlimit(1000000)
 
-    res = -1
-    # If the value of coin at current location
-    # is greater than amount
-    if coins[idx] > amount:
-        # Case where we are not taking the current coin
-        doNotTakeCoin = coinChange(idx+1, coins, amount)
-        res = doNotTakeCoin
-    else:
-        # Two cases are there
-        # case-1: where we take the coin
-        # case-2: where we do not take the coin
-        takeCoin = 1 + coinChange(idx, coins, amount-coins[idx])
-        doNotTakeCoin = 0 + coinChange(idx+1, coins, amount)
-        res = min(takeCoin, doNotTakeCoin)
+def minPathSum(i, j, grid, dp):
+    # Base case-1
+    if i == 0 and j == 0:
+        return grid[i][j]
 
-    return res
+    # Base case-2
+    if i < 0 or j < 0:
+        return 10000007
 
-if __name__ == "__main__":
-    n, amount = map(int, input().split())
-    coins = list(map(int, input().split()))
-    print(coinChange(0, coins, amount))
+    # Memoization case
+    if dp[i][j] != -1:
+        return dp[i][j]
+
+    # Recur for the path in 0 to i-1
+    up = grid[i][j] + minPathSum(i-1, j, grid, dp)
+
+    # Recur for the path in 0 to j-1
+    left = grid[i][j] + minPathSum(i, j-1, grid, dp)
+
+    # Return the result
+    dp[i][j] = min(up, left)
+    return dp[i][j]
+
+n, m = map(int, input().split())
+grid = []
+for i in range(n):
+    row = list(map(int, input().split()))
+    grid.append(row)
+
+dp = [[-1 for j in range(m)] for i in range(n)]
+print(minPathSum(n-1, m-1, grid, dp))
