9de65ff96800687711d3db35e5c3df5a16636eae
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-03-20 12:20:09 +0530

    Update mergeIntervals.cpp

--- mergeIntervals.cpp
+++ mergeIntervals.cpp
@@ -5,22 +5,23 @@ using namespace std;
 class Solution {
 public:
      vector<vector<int>>mergeIntervals(vector<vector<int>>intervals){
-           //find the size of the intervals
+         //find the size of the intervals
            int n = intervals.size();
+
+           
 
            //Sort the given array of intervals
            sort(intervals.begin(),intervals.end());
+
 
            //find the starting and ending indices of the interval
            int l = intervals[0][0];
            int r = intervals[0][1];
 
-          
             //result array to store the final intervals
             vector<vector<int>>res;
 
-
-           //Start iterating from the second interval 
+            //Start iterating from the second interval 
            for(int i=1;i<n;i++){
             
                //check if the starting point of the
@@ -37,12 +38,15 @@ public:
                    r = max(intervals[i][1],r);
                }
            }
-  
+
+
             //push the final interval into the array
            res.push_back({l,r});
-   
+
+
            //return the result
            return res;
      }
 };
+
 
