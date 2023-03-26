22256f6f37f7370f0eae5d44ef6e92d54b8858b2
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-03-26 20:46:08 +0530

    Update LRUCache.py

--- LRUCache.py
+++ LRUCache.py
@@ -1,26 +1,32 @@
-# Function to get the recently used item 
-def get(self, key: int) -> int:
-    # if the cache does not contains the key, then return -1
-    if key not in self.mp: 
-        return -1
+from typing import List, Dict, Tuple
 
-    # set this key as the most recently used item
-    makeRecentlyUsed(key)
+class LRUCache:
 
-    # return the item in the linked list
-    return self.mp.get(key)[1]
+    def __init__(self, capacity: int):
+        self.n = capacity
+        self.dll = []
+        self.mp = {}
 
-    # If the capacity has been extended
-    if self.n < 0:
-        # Find the key to be deleted
-        # Least recently used key has to be deleted
-        key_to_be_deleted = self.dll[-1]
+    def makeRecentlyUsed(self, key: int) -> None:
+        self.dll.remove(self.mp[key][0])
+        self.dll.insert(0, key)
+        self.mp[key] = (self.dll.index(key), self.mp[key][1])
 
-        # Pop that key from the list
-        self.dll.pop()
+    def get(self, key: int) -> int:
+        if key not in self.mp:
+            return -1
+        self.makeRecentlyUsed(key)
+        return self.mp[key][1]
 
-        # Erase the pointer in the hashmap
-        del self.mp[key_to_be_deleted]
-
-        # Increment the given capacity by one
-        self.n += 1
+    def put(self, key: int, value: int) -> None:
+        if key in self.mp:
+            self.mp[key] = (self.mp[key][0], value)
+            self.makeRecentlyUsed(key)
+        else:
+            self.dll.insert(0, key)
+            self.mp[key] = (self.dll.index(key), value)
+            self.n -= 1
+        if self.n < 0:
+            keyToBeDeleted = self.dll.pop()
+            del self.mp[keyToBeDeleted]
+            self.n += 1
