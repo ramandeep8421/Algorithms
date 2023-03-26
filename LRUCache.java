d23fe3a8a7f0e470fb3cfd43044cd6614aba101c
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-03-26 20:47:21 +0530

    Update LRUCache.java

--- LRUCache.java
+++ LRUCache.java
@@ -1,41 +1,45 @@
-// Function to put the item on the top
-private void makeRecentlyUsed(int key){
-    dll.erase(mp.get(key).first);
-    dll.push_front(key);
-    mp.get(key).first = dll.begin();
-}
+import java.util.*;
 
-// Constructor to initialize the n with capacity
-LRUCache(int capacity){
-    n = capacity;
-    dll = new LinkedList<>();
-    mp = new HashMap<>();
-}
+class LRUCache {
 
-// Function to get the recently used item 
-int get(int key){
-    // if the cache does not contains the key, then return -1
-    if(!mp.containsKey(key)) return -1;
+    private int n;
+    private List<Integer> dll;
+    private Map<Integer, Pair<List<Integer>.Iterator, Integer>> mp;
 
-    // set this key as the most recently used item
-    makeRecentlyUsed(key);
+    public LRUCache(int capacity) {
+        this.n = capacity;
+        this.dll = new LinkedList<>();
+        this.mp = new HashMap<>();
+    }
 
-    // return the item in the linked list
-    return mp.get(key).second;
+    private void makeRecentlyUsed(int key){
+        dll.remove(mp.get(key).getKey());
+        dll.add(0, key);
+        mp.put(key, new Pair<>(dll.listIterator(), mp.get(key).getValue()));
+    }
 
-    // If the capacity has been extended
-    if(n < 0){
-        // Find the key to be deleted
-        // Least recently used key has to be deleted
-        int key_to_be_deleted = dll.back();
+    public int get(int key) {
+        if(!mp.containsKey(key)) {
+            return -1;
+        }
+        makeRecentlyUsed(key);
+        return mp.get(key).getValue();
+    }
 
-        // Pop that key from the list
-        dll.pop_back();
-
-        // Erase the pointer in the hashmap
-        mp.remove(key_to_be_deleted);
-
-        // Increment the given capacity by one
-        n++;
+    public void put(int key, int value) {
+        if(mp.containsKey(key)) {
+            mp.get(key).setValue(value);
+            makeRecentlyUsed(key);
+        } else {
+            dll.add(0, key);
+            mp.put(key, new Pair<>(dll.listIterator(), value));
+            n--;
+        }
+        if(n < 0) {
+            int keyToBeDeleted = dll.remove(dll.size() - 1);
+            mp.remove(keyToBeDeleted);
+            n++;
+        }
     }
 }
+
