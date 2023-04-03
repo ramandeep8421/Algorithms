6f86629625e0e16b24ef3e4c07e55d812dcfb84f
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-04-02 19:31:36 -0700

    Update substringWithConcatenationOpt.cpp

--- substringWithConcatenationOpt.cpp
+++ substringWithConcatenationOpt.cpp
@@ -1,118 +1,80 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-class substringWithConcatenation{
 
-public:
-    //Hashmap to store the 
-    //count of the words
-	map<string,int>wordCount;
+class Solution {
+private:
+    std::unordered_map<std::string, int> wordCount;
+    int n;
+    int wordLength;
+    int substringSize;
+    int k;
 
-	//size of the string
-	int n;
+    void slidingWindow(int left, std::string s, std::vector<int>& answer) {
+        std::unordered_map<std::string, int> wordsFound;
+        int wordsUsed = 0;
+        bool excessWord = false;
 
-	//variable representing the wordlength
-	int wordLength;
+        // Do the same iteration pattern as the previous approach - iterate
+        // word_length at a time, and at each iteration we focus on one word
+        for (int right = left; right <= n - wordLength; right += wordLength) {
 
-	//Substring size 
-	int substringSize;
+            std::string sub = s.substr(right, wordLength);
+            if (wordCount.find(sub) == wordCount.end()) {
+                // Mismatched word - reset the window
+                wordsFound.clear();
+                wordsUsed = 0;
+                excessWord = false;
+                left = right + wordLength;
+            } else {
+                // If we reached max window size or have an excess word
+                while (right - left == substringSize || excessWord) {
+                    std::string leftmostWord = s.substr(left, wordLength);
+                    left += wordLength;
+                    wordsFound[leftmostWord]--;
 
-	//size of the words array
-	int k;
+                    if (wordsFound[leftmostWord] >= wordCount[leftmostWord]) {
+                        // This word was an excess word
+                        excessWord = false;
+                    } else {
+                        // Otherwise we actually needed it
+                        wordsUsed--;
+                    }
+                }
 
-	   //Function which finds the starting indices of the valid substrings
-	void slidingWindow(int left, string s, vector<int>&answer){
-         
-       //temporary hashmap 
-		map<string,int>wordsPresent;
+                // Keep track of how many times this word occurs in the window
+                wordsFound[sub]++;
+                if (wordsFound[sub] <= wordCount[sub]) {
+                    wordsUsed++;
+                } else {
+                    // Found too many instances already
+                    excessWord = true;
+                }
 
-		//variable to count the number of words used in the 
-		//current window
-		int wordsFound = 0;
-
-			//Boolean variable to check for the given
-		// if we have an excess word or not
-		bool excessWord = false;
-
-		//Iterate wordLength at a time 
-		for(int right = left; right <= n-wordLength; right += wordLength){
-                  //Find the substring of size wordLength
-			     string sub = s.substring(right,right+wordLength);
-
-                   //If we have sub as the word count
-			     if(!wordCount[sub]){
-			     	 wordsPresent.clear();
-			     	 wordsFound = 0;
-			     	 excessWord = false;
-			     	 left = left + wordLength;
-			     }
-			     else{
-			     	 //If we reach max window size or have an excess word
-			     	 while(right - left == substringSize || excessWord){
-			     	 	  string leftmostWord = s.substring(left,left+wordLength);
-			     	 	  left += wordLength;
-			     	 	  wordsCount[leftMostWord]--;
-
-			     	 	  if(wordsCount[leftmostWord] >= wordsPresent[leftmostWord]){
-			     	 	  	    //This word was an excess word
-                                 excessWords = false;
-			     	 	  }else{
-			     	 	  	     //Otherwise we needed it 
-                                 wordsFound--;
-			     	 	  }
-			     	 }
-
-			     	 //Keep track of how many times 
-			     	 // this word exists in the given window
-			     	 wordPresent[sub]++;
-
-			     	 if(wordPresent[sub] <= wordCount[sub]){
-                         wordsFound++;
-			     	 }else{
-			     	 	//Found too many instances already
-			     	 	excessWord = true;
-
-			     	 }
-
-			     	 if(wordsFound == k && !excessWord){
-			     	 	 //Found a valid substring 
-			     	 	 answer.push_back(left);
-			     	 }
-
-			     }
-
-		}
-
-
-	}
-
-	//Find Substring Function
-    vector<int>findSubstring(string s, vector<string>&words){
-    	   //Find the length of of given string s
-           n = s.length();
-
-           //Length of the words
-           k = words.size();
-
-           //Size of each wrd
-           wordLength = words[0].size();
-
-           //Size of the substring
-           substringSize = wordLength * k;
-
-           //Iterate over the given words array
-           // and make entries in it
-           for(auto word: words){
-           	  wordFound[word]++;
-           }
-
-           //Call the slidingWindow function wordLength times
-           for(int i=0;i<wordLength;i++){
-           	  slidingWindow(i,s,answer);
-           }
-
-           return answer;
+                if (wordsUsed == k && !excessWord) {
+                    // Found a valid substring
+                    answer.push_back(left);
+                }
+            }
+        }
     }
 
+public:
+    std::vector<int> findSubstring(std::string s, std::vector<std::string>& words) {
+        n = s.length();
+        k = words.size();
+        wordLength = words[0].length();
+        substringSize = wordLength * k;
 
-}
+        for (std::string word : words) {
+            wordCount[word]++;
+        }
+
+        std::vector<int> answer;
+        for (int i = 0; i < wordLength; i++) {
+            slidingWindow(i, s, answer);
+        }
+
+        return answer;
+    }
+};
