399f79db79a76690d21d3d6ca91590f55620e991
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-04-11 21:24:10 -0700

    Update uniquePaths3.cpp

--- uniquePaths3.cpp
+++ uniquePaths3.cpp
@@ -2,96 +2,66 @@
 using namespace std;
 
 class Solution {
-private:
-    //variable to store the answer
-    int ans = 0;
-
-    //Direction vector
-    vector<int> dr = {0, 1, 0, -1, 0};
-
-    //dfs function to traverse the grid
-    void dfs(int i, int j, int x, int y, vector<vector<int>>& grid, vector<vector<bool>>& visited) {
-        //dimensions of the grid
-        int n = grid.size(), m = grid[0].size();
-
-        //check if the current cell meets the ending cell
-        if (i == x && j == y) {
-            //mark this cell as visited
-            visited[i][j] = true;
-
-            //check if all cells of the grid are visited or not
-            if (check(grid, visited)) {
-                ans++;
-            }
-
-            //unmark the current cell
-            visited[i][j] = false;
-
-            //return from here
-            return;
-        }
-
-        //mark the visited true
-        visited[i][j] = true;
-
-        //move to 4 directions
-        for (int k = 0; k < 4; k++) {
-            //move to new cell
-            int newr = i + dr[k], newc = j + dr[k + 1];
-
-            //If the new cell is already visited or its not an empty cell then continue to another cell
-            if (newr < 0 || newr >= n || newc < 0 || newc >= m || grid[newr][newc] == 1 || visited[newr][newc]) {
-                continue;
-            }
-
-            //move to the new cell
-            dfs(newr, newc, x, y, grid, visited);
-        }
-
-        //after exploring the paths in all 4 directions
-        //unmark the current cell
-        visited[i][j] = false;
-    }
-
 public:
-    int uniquePathsIII(vector<vector<int>>& grid) {
-        //find the dimensions of the grid
-        int n = grid.size(), m = grid[0].size();
+      //variable to store the final answer
+    int ans=0;
 
-        //declare a visited array
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+    //direction vectors
+    vector<int> dr={-1,0,1,0};
+    vector<int> dc={0,1,0,-1};
 
-        int sr = 0, sc = 0, er = 0, ec = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    sr = i;
-                    sc = j;
-                } else if (grid[i][j] == 2) {
-                    er = i;
-                    ec = j;
-                }
-            }
-        }
-
-        //call the dfs function at the starting cell
-        //or the top left cell
-        dfs(sr, sc, er, ec, grid, visited);
-
-        //return the answer from the end of the function
-        return ans;
-    }
-
-    //helper function to check if all empty cells are visited or not
-    bool check(vector<vector<int>>& grid, vector<vector<bool>>& visited) {
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] == 0 && !visited[i][j]) {
-                    return false;
-                }
+     //fundtion to check for the visited cells
+    bool check(vector<vector<int>> &grid, vector<vector<bool>> &vis){
+        int n= grid.size();
+        int m=grid[0].size();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==0 && vis[i][j]==false) return false;
             }
         }
         return true;
     }
+
+      //dfs function to perform the traversal
+    void dfs(int i, int j, int x, int y, vector<vector<int>> &grid, vector<vector<bool>> &vis){
+        int n= grid.size();
+        int m=grid[0].size();
+
+        if(i==x && j==y){
+            vis[i][j]=true;
+            if(check(grid,vis)) ans++;
+            vis[i][j]=false;
+            return;
+        }
+        vis[i][j]=true;
+        for(int k=0; k<4; k++){
+            int newr= i+dr[k], newc= j+dc[k];
+            if(newr<0 || newr>=n || newc<0 || newc>=m || grid[newr][newc]==-1 || vis[newr][newc]) continue;
+            dfs(newr,newc,x,y,grid,vis);
+        }
+        vis[i][j]=false;
+    }
+
+    
+    //function to count the unique paths
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        int n= grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
+
+        int sr,sc,er,ec;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1){
+                    sr=i, sc=j;
+                }
+                else if(grid[i][j]==2){
+                    er=i, ec=j;
+                }
+            }
+        }
+        dfs(sr,sc,er,ec,grid,vis);
+        return ans;
+    }
+
 };
