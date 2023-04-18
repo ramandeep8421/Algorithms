15d718f5f7841a7ca21ceef58cb2b61c374cfb51
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-04-18 07:07:50 -0700

    Create hitCounter.py

---
+++ hitCounter.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+class HitCounter:
+    def __init__(self):
+        self.hits = deque()
+
+    def hit(self, timestamp: int) -> None:
+        self.hits.append(timestamp)
+
+    def getHits(self, timestamp: int) -> int:
+        while self.hits and self.hits[0] <= timestamp - 300:
+            self.hits.popleft()
+        return len(self.hits)
