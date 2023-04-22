cadcd8962a342efa30adbda207f780ba87e12b7c
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-04-21 20:22:51 -0700

    Update LCA.cpp

--- LCA.cpp
+++ LCA.cpp
@@ -1,3 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//Definition for treeNode
+struct TreeNode{
+   int val;
+   TreeNode*left;
+   TreeNode*right;
+   TreeNode(int x): val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* ans;
