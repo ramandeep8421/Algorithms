68a56900a372c014b2efc573c02f6a89d6b4cd72
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-05-08 06:10:37 -0700

    Create minNumberOperations.cpp

---
+++ minNumberOperations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+        int minNumberOperations(vector<int>&target){
+             int n = target.size();
+             int prev = target[0];
+             int ans = prev;
+
+             for(int i = 1;i < n; i++){
+                if(target[i] > prev){
+                    ans += (target[i] - prev);
+                }
+                prev = target[i];
+             }
+
+             return ans;
+        }
+};
