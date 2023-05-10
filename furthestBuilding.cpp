d569e66f6cc86b60be00587fa199b6fca571f33a
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-05-10 07:44:18 -0700

    Create furthestBuilding.cpp

---
+++ furthestBuilding.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool check(vector<int> heights, int bricks, int ladders, int mid){
+       vector<int> diff;
+        for(int i = 1; i <= mid; i++){
+            if(heights[i] > heights[i-1]){
+                diff.push_back(heights[i]-heights[i-1]);
+            }
+        }
+
+          sort(diff.begin(), diff.end(), greater<int>());
+        int l = diff.size();
+
+         for(int i = ladders; i < l; i++){
+            if(diff[i] > bricks) return false;
+            bricks -= diff[i];
+        }
+        return true;
+
+    }
+    
+    
+    int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
+          int n = heights.size();
+        int lo = 0, hi = n-1;
+
+         while(lo < hi){
+            int mid = (lo+hi+1)/2;
+            if(check(heights, bricks, ladders, mid)){
+                lo = mid;  // if true, then binary search on right half
+            }
+            else{
+                hi = mid-1; // if false, then binary search on left half
+            }
+        }
+
+        return lo;
+    }
+};
