0444fe5079e248f3755ab64a6a95a91eb9fbd163
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-05-28 03:56:19 -0700

    Create buyandsellstock.java

---
+++ buyandsellstock.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        
+        int mini = prices[0];
+        int maxi = prices[1];
+        int ans = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (prices[i] < mini) {
+                mini = prices[i];
+                maxi = prices[i];
+            }
+            
+            maxi = Math.max(maxi, prices[i]);
+            ans = Math.max(ans, maxi - mini);
+        }
+        
+        return ans;
+    }
+}
+
+           
+
+           
+
+
+ 
