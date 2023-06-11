704d9c57dc66a38457beceb2f218ef6ac4b07405
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-06-11 11:52:10 -0700

    Create lowerBound.py

---
+++ lowerBound.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lower_bound(self, n, a, target):
+        lo = 0
+        hi = n - 1
+        ans = n
+
+        while lo <= hi:
+            mid = lo + (hi - lo) // 2
+
+            if a[mid] >= target:
+                ans = mid
+                hi = mid - 1
+            else:
+                lo = mid + 1
+
+        return ans
