36a4afe8673a6c165c1986485e33af67acab007d
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-06-27 05:14:01 -0700

    Update breakTheTree.cpp

--- breakTheTree.cpp
+++ breakTheTree.cpp
@@ -1,64 +1,70 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+#include<bits/stdc++.h>
+using namespace std;
 
-class Solution {
-    public long max(long a, long b) {
-        return (a > b) ? a : b;
-    }
 
-    public int dfs(int mid, int node, int par, ArrayList<ArrayList<Integer>> graph, int[] wt, int[] tot, int c) {
-        tot[node] = wt[node];
+class Solution
+{
+    public:
 
-        ArrayList<Integer> arr = new ArrayList<>();
-
-        for (int it : graph.get(node)) {
-            if (it != par) {
-                arr.add(dfs(mid, it, node, graph, wt, tot, c));
-            }
+        long long max(long long a, long long b){
+            return (a > b) ? a : b;
         }
 
-        Collections.sort(arr);
+        void dfs(int mid,int node , int par, vector<vector<int>>&graph, vector<int>&tot, int c){
+              tot[node] = wt[node];
 
-        for (int it : arr) {
-            if (tot[node] + it > mid) {
-                c++;
-            } else {
-                tot[node] += it;
-            }
+              vector<int>arr;
+
+              for(auto it: graph[node]){
+                  if(it != par){
+                      arr.push_back(dfs(it, node, graph, tot, c));
+                  }
+              }
+
+              sort(arr.begin(),arr.end());
+
+              for(auto it: arr){
+
+                   if(tot[node] + it > mid)
+                      c++;
+                    else
+                       tot[node] += it;
+              }
+
+              return tot[node];
         }
 
-        return tot[node];
-    }
+        long long minimumWeightSubtree(vector<int>&wt, vector<vector<int>>&graph, int n, int k){
+             vector<int>tot;
+             tot.resize(n+1);
 
-    public long minimumWeightSubtree(int[] wt, ArrayList<ArrayList<Integer>> graph, int n, int k) {
-        int[] tot = new int[n + 1];
+             long long mn = -1, mx = 0;
 
-        long mn = -1, mx = 0;
+             for(auto it: wt){
+                 mn = max(mn,it);
+                 mx += it;
+             }
 
-        for (int it : wt) {
-            mn = max(mn, it);
-            mx += it;
+             long long ans;
+             long long lo = mn, hi = mx;
+          
+             int c = 0;
+
+             while(lo <= hi){
+
+                 long long mid = lo + (hi - lo)/2;
+                 
+                 dfs(mid,1,0,graph, wt,tot,c);
+
+                 if(c <= k-1){
+                    ans = mid;
+                    hi = mid-1;
+                 }else{
+                     lo = mid+1;
+                 }
+
+             }
+
+             return ans;
         }
-
-        long ans;
-        long lo = mn, hi = mx;
-
-        int c = 0;
-
-        while (lo <= hi) {
-            long mid = lo + (hi - lo) / 2;
-
-            dfs(mid, 1, 0, graph, wt, tot, c);
-
-            if (c <= k - 1) {
-                ans = mid;
-                hi = mid - 1;
-            } else {
-                lo = mid + 1;
-            }
-        }
-
-        return ans;
-    }
-}
+};
