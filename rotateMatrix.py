b7f40bc96aa544a33b679d58cfb92aa02e357a3e
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-07-27 08:41:33 -0700

    Create rotateMatrix.py

---
+++ rotateMatrix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix):
+        n = len(matrix)
+
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row to get the rotated matrix
+        for i in range(n):
+            for j in range(n // 2):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
