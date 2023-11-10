c9706d161b38fa190a74dbc40d87ca166c7d420b
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-11-09 22:40:19 -0800

    Create invertTree.java

---
+++ invertTree.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        invert(root);
+        return root;
+    }
+
+    private void invert(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        TreeNode temp = node.left;
+        node.left = node.right;
+        node.right = temp;
+
+        invert(node.left);
+        invert(node.right);
+    }
+}
