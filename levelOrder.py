7a1d7c5b0885ed44c36b0a4f0b1c375cc14ba07d
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2023-11-10 23:32:20 -0800

    Create levelOrder.py

---
+++ levelOrder.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            level_size = len(queue)
+
+            for i in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+
+        return result
