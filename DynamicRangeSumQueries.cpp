a4ea7660d13d0f52dfbe2350a640b5791f77acf2
Ramandeep singh <55442782+ramandeep8421@users.noreply.github.com>
2026-04-09 18:11:04 +0530

    Create DynamicRangeSumQueries.cpp

---
+++ DynamicRangeSumQueries.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+const int MAXN = 200011;
+int n, t[4*MAXN];
+
+void build(int a[], int v, int tl, int tr) {
+    if (tl == tr) {
+        t[v] = a[tl];
+    } else {
+        int tm = (tl + tr) / 2;
+        build(a, v*2, tl, tm);
+        build(a, v*2+1, tm+1, tr);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+
+int sum(int v, int tl, int tr, int l, int r) {
+    if (l > r) 
+        return 0;
+    if (l == tl && r == tr) {
+        return t[v];
+    }
+    int tm = (tl + tr) / 2;
+    return sum(v*2, tl, tm, l, min(r, tm))
+           + sum(v*2+1, tm+1, tr, max(l, tm+1), r);
+}
+
+void update(int v, int tl, int tr, int pos, int new_val) {
+    if (tl == tr) {
+        t[v] = new_val;
+    } else {
+        int tm = (tl + tr) / 2;
+        if (pos <= tm)
+            update(v*2, tl, tm, pos, new_val);
+        else
+            update(v*2+1, tm+1, tr, pos, new_val);
+        t[v] = t[v*2] + t[v*2+1];
+    }
+}
+
+int32_t main() {
+	
+	int n,q;
+	cin >> n >> q;
+	
+	int x[n];
+	
+	for(int i=0;i<n;i++)cin >> x[i];
+	
+	build(x,1,0,n-1);
+	
+	while(q--){
+	     
+	     int t,a,b;
+	     cin >> t >> a >> b;
+	     
+	     a--;
+	     
+	     if(t == 1){
+	         x[a]=b;
+	         update(1,0,n-1,a,b);
+	     }
+	     else{
+	          b--;
+	          int ans = sum(1,0,n-1,a,b);
+	          cout << ans << "\n";
+	     }
+	}
+	
+
+}
